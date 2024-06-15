262d76f92b318bb95caef907f5394c0e1adc3df7
maniratnam-1 <mani.ashratech@gmail.com>
2024-06-16 00:59:17 +0530

    Update sreamlit_app.py

--- sreamlit_app.py
+++ sreamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import requests
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -22,28 +23,22 @@ name_on_order = st.text_input("Name on Smoothie")
 st.write("The name on your smoothie will be:", name_on_order)
 
 if ingredients_list:
-#    st.write(ingredients_list)
-#    st.text(ingredients_list)
-
     ingredients_string=''
-
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + " "
-
-    #st.write(ingredients_string)
-
-
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df =st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+    
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
 
     time_to_insert = st.button("Submit Order")
+    
 
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
-    import requests
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-    #st.text(fruityvice_response.json())
-    fv_df =st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+    
+    
 st.write(my_insert_stmt)
 st.stop()
 
